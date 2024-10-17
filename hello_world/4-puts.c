c071ec2ff5277858e4768d6a09c1d5a0c1329edc
Emimoreira <10152@holbertonstudents.com>
2024-10-17 04:27:59 +0000

    commit_message

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,13 +2,11 @@
 /**
  * main - Entry point
  *
- * Description: Calls puts and recieves value returned by puts after its
- * execution.
- * Return: Always return 0 to indicate success
+ * Return: Always 0 (Success)
+ * Description - Printing lines
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
