111803049abd7f3b99c0ff86400c007b3e07d091
Emimoreira <10152@holbertonstudents.com>
2024-10-18 14:54:38 +0000

    commit_message

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	printf("ingresa un numero: ");
+	scanf("%d",&n);
+
+	int (last_digit = n% 10 );
+        
+	if (last_digit > 5) {
+		printf("la cadena y es mayor que 5\n");
+	} else if (last_digit == 0) {
+	        printf("la cadena y es 0\n");
+	} else {
+		printf("la cadeba y es menor que 6 y no 0\n");
+	}
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2; 	
+	return (0);
+}
