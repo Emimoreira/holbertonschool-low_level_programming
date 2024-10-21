079329d03242c5da600d5dfeb61b318d50164197
Emimoreira <10152@holbertonstudents.com>
2024-10-21 14:26:06 +0000

    commit_message

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - sirve para printear en minuscula todo hasta base 16
+ * Return : 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else 
+		{
+			putchar(i - 10 + 'a');
+		}
+	}	
+	putchar('\n');
+	return 0;
+}
