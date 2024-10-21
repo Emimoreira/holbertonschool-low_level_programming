97f7ab64a1526f77bf8af995f6e2a3454e851d82
Emimoreira <10152@holbertonstudents.com>
2024-10-21 14:01:06 +0000

    commit_message

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,19 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * 6-print_numberz.c - cuenta todo lo que tenga solo un digito  de base 10 desade 0
+ * 6-print_numberz.c - cuenta todo lo que tenga solo un digito de base 10
  *
  *Return: 0
  */
 int main(void)
 {
 	int i;
-	for ( i= 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 	putchar(i + '0');
 	}
 
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
