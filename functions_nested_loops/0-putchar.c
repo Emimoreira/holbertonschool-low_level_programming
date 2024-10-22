3fdc8f5b5327e74eb00d4f0e74e91397f5b1a28f
Emimoreira <10152@holbertonstudents.com>
2024-10-22 13:12:53 +0000

    commit_message

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "putchar.c"
 /**
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
@@ -15,4 +16,4 @@ int main(void)
     _putchar('\n');
 
     return (0);
-}                                                                                                                                                                                                                                                                          
+}
