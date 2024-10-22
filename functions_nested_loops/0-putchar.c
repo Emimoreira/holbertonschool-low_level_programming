b57502c225cf76b45fad9d5cc3631f9f6a54784e
Emimoreira <10152@holbertonstudents.com>
2024-10-22 12:32:13 +0000

    commit_message

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,17 +2,10 @@
 /**
  *main - programa que imprime _putchar
  *Return: 0
- */ 
+ */
 int main(void)
 {
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r'); 
-	putchar('\n');
-
+	printf("_putchar\n");
 	return 0;
 }
+~                                                                                                                                                                                                                                                                           
