92b668794799b62495b3f26630dd95b3a01d1202
Emimoreira <10152@holbertonstudents.co>
2024-10-30 13:35:18 +0000

    commit_message

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,29 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-/** * rev_string - Esta función imprime un inversor de cadena 
- * * *@s: este es el puntero que apunta a una cadena
+/** * rev_string - Reverses a string
+ *  * @s: Input string
+ *   * Return: String in reverse
  */
-
 void rev_string(char *s)
 {
-	int a, len;
-
-	char *begin, *end = s;
-
-	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
+	char rev = s[0];
+	int counter = 0;
+	int i;
+	while (s[counter] != '\0')counter++;
+	for (i = 0; i < counter; i++)
 	{
-		end++;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
-	len = a + 1;
-	begin = s;
-	for (a = 0; a < len / 2; a++)
-	{
-		char x;
-		x = *end;
-		*end = *begin;
-		*begin = x;
-		begin++;
-		end--;
-	}
-	end[len + 1] = '\0';
 }
