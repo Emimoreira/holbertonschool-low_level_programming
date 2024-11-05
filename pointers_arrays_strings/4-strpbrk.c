b8e1e8aa72aa2d9673f09f6b529a10dae1bb6e38
Emimoreira <10152@holbertonstudents.com>
2024-11-05 15:04:23 +0000

    commit_message

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strpbrk - busca el primer caracter de accept
+* @s:s
+* @a:a
+*Return: nada
+*/
+
+char *_strpbrk(char *s, char *accept);
+{
+
+while (*s)
+	{
+	char *a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				return s;
+			}
+			a++
+		}
+		s++
+	}
+	return NULL
+}
