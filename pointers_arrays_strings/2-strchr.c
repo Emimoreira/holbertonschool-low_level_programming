7094c70fcc3e4fa018d0feadc436bf3105d259a5
Emimoreira <10152@holbertonstudents.com>
2024-11-05 13:15:27 +0000

    commit_message

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strchr - Returns a pointer to the first occurrence
+*@s:string targeted
+*@c:character targeted
+*Return: returns pointer to first occcurence of c
+*/
+char *_strchr(char *s, char c)
+{
+	int e;
+
+	for (e = 0; (s[e] != c) && (s[e] != '\0'); e++)
+		;
+	if (s[e] == c)
+		return (s + e);
+	else
+		return (NULL);
+}
