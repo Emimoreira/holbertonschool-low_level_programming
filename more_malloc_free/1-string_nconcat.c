2c942860faa710ae54d22c13d02ffe91de19d5cf
Emimoreira <10152@holbertonstudents.com>
2024-11-14 17:33:17 +0000

    commit_message

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,35 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int len1 = 0, len2 = 0, i, j;
+	char *result;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
+	if (s1 == NULL)
+	s1 = "";
 
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+	s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
+	while (s1[len1] != '\0')
+	len1++;
 
-    while (s2[len2] != '\0')
-        len2++;
+	while (s2[len2] != '\0')
+	len2++;
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+	n = len2;
 
-    result = malloc(len1 + n + 1);
-    if (result == NULL)
-        return (NULL);
+	result = malloc(len1 + n + 1);
+	if (result == NULL)
+	return (NULL);
 
-    for (i = 0; i < len1; i++)
-        result[i] = s1[i];
+	for (i = 0; i < len1; i++)
+	result[i] = s1[i];
 
-    for (j = 0; j < n; j++)
-        result[i + j] = s2[j];
+	for (j = 0; j < n; j++)
+	result[i + j] = s2[j];
 
-    result[i + j] = '\0';
+	result[i + j] = '\0';
 
-    return (result);
+	return (result);
 }
