dda830880bd6f062dede64521d240f9f1b1dbe3c
Emimoreira <10152@holbertonstudents.com>
2024-11-26 18:26:12 +0000

    commit_message

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,22 +1,21 @@
 #include <stddef.h>
 #include "function_pointers.h"
 /**
- * print_name - function to print name
+ * array_iterator - function to print name
  * @name: pointer of char type
  * @f: pointer to function
  * Return: Always successful
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int));
-
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	site_t i;
+	size_t i;
 
-	if (array == NULL || ation == NULL)
+	if (array == NULL || action == NULL)
 	{
 		return;
 	}
-	for (1 = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 
