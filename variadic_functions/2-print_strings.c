7e191f359ceb484eb257ee49c1c978c6adead2bf
Emimoreira <10152@holbertonstudents.com>
2024-12-16 16:11:28 +0000

    commit_message

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -7,7 +7,7 @@
  *@n: cantidad de strings
  *Return: string separador string
  */
-void print_strings(const char *separator, const unsigned int n, ...) 
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
@@ -15,21 +15,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i = 0; i < n; i++) 
+	for (i = 0; i < n; i++)
 	{
 	str = va_arg(args, const char *);
 
-	if (str == NULL) 
+	if (str == NULL)
 	{
 	printf("(nil)");
-	} 
-	else 
+	}
+	else
 	{
 		printf("%s", str);
 	}
 
-        
-	if (i != n - 1 && separator != NULL) 
+	if (i != n - 1 && separator != NULL)
 	{
 		printf("%s", separator);
 	}
