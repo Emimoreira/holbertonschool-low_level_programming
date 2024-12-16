85746e4b1a5b7e54113ebaeb01278f98dc083277
Emimoreira <10152@holbertonstudents.com>
2024-12-16 16:41:43 +0000

    commit_message

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,64 +1,51 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
-/**
- *print_all- funcion que printea
- *@format: el formato a imprimir
- *
- */
 
-void print_all(const char * const format, ...) 
-{
-	va_list args;
-	unsigned int i = 0;
-	char *str;
-	char c;
-	int d;
-	float f;
+void print_all(const char * const format, ...) {
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    char c;
+    int d;
+    float f;
 
-	va_start(args, format);
-	while (format && format[i]) 
-	{
+    va_start(args, format);
 
-	if (i != 0) 
-	{
-		printf(", ");
-	}
+    while (format && format[i]) {
 
-        if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') 
-		{
-		if (format[i] == 'c')
-		{
-		c = va_arg(args, int);
-		printf("%c", c);
-		} 
-		else if (format[i] == 'i') 
-		{
-		d = va_arg(args, int);
-		printf("%d", d);
-		} 
-		else if (format[i] == 'f') 
-		{
-		f = va_arg(args, double);
-		printf("%f", f);
-		} 
-		else if (format[i] == 's') 
-		{
-		str = va_arg(args, char *);
-		if (str == NULL) 
-		{
-		printf("(nil)");
-		}
-		else 
-		{
-		printf("%s", str);
-		}
-		}
-	}
+        if (i != 0) {
+            printf(", ");
+        }
 
-	i++;
-	}
-	printf("\n");
 
-	va_end(args);
+        if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') {
+
+            if (format[i] == 'c') {
+                c = va_arg(args, int);
+                printf("%c", c);
+            }
+            if (format[i] == 'i') {
+                d = va_arg(args, int);
+                printf("%d", d);
+            }
+            if (format[i] == 'f') {
+                f = va_arg(args, double);
+                printf("%f", f);
+            }
+            if (format[i] == 's') {
+                str = va_arg(args, char *);
+                if (str == NULL) {
+                    printf("(nil)");
+                } else {
+                    printf("%s", str);
+                }
+            }
+        }
+
+        i++;
+    }
+
+    printf("\n");
+
+    va_end(args);
 }
