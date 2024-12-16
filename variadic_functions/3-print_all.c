c2a7d7f2ded7cb2169436b6aeef2a86411aa5e3b
Emimoreira <10152@holbertonstudents.com>
2024-12-16 16:36:38 +0000

    commit_message

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,44 +17,48 @@ void print_all(const char * const format, ...)
 	float f;
 
 	va_start(args, format);
-
 	while (format && format[i]) 
 	{
-	if (i != 0)
+
+	if (i != 0) 
 	{
-	printf(", ");
+		printf(", ");
 	}
 
-	if (format[i] == 'c') 
-	{
+        if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') 
+		{
+		if (format[i] == 'c')
+		{
 		c = va_arg(args, int);
 		printf("%c", c);
-	}
-       	else if (format[i] == 'i')
-       	{
-	d = va_arg(args, int);
-	printf("%d", d);
-	} 
-	else if (format[i] == 'f') 
-	{
-	f = va_arg(args, double);
-	printf("%f", f);
-	} 
-	else if (format[i] == 's') 
-	{
-	str = va_arg(args, char *);
-	if (str == NULL) 
-	{
+		} 
+		else if (format[i] == 'i') 
+		{
+		d = va_arg(args, int);
+		printf("%d", d);
+		} 
+		else if (format[i] == 'f') 
+		{
+		f = va_arg(args, double);
+		printf("%f", f);
+		} 
+		else if (format[i] == 's') 
+		{
+		str = va_arg(args, char *);
+		if (str == NULL) 
+		{
 		printf("(nil)");
-	} 
-	else 
-	{
+		}
+		else 
+		{
 		printf("%s", str);
+		}
+		}
 	}
-	}
+
 	i++;
 	}
-    	printf("\n");
+	printf("\n");
 
-    	va_end(args);
+	va_end(args);
 }
